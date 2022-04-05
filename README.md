a6ebefa96be13576e0b0fe41af312a77eaa44d94
JimiIT92 <jimiit92@gmail.com>
2022-04-06 01:40:56 +0200

    Add "about" page and image credits

---
+++ README.md
@@ -0,0 +1,3 @@
+<a href="https://www.flaticon.com/free-icons/ball" title="ball icons">Ball icons created by Pause08 - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/concert" title="concert icons">Concert icons created by Smashicons - Flaticon</a>
+<a href="https://www.hiclipart.com/free-transparent-background-png-clipart-vzwzc" title="nerd herd">Nerd Herd icon created by HiClipart</a>
--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,16 @@
 const navBtn = document.querySelector(".nav__btn");
 const navLinks = document.querySelector(".nav__links");
+const navBar = document.querySelector(".navbar");
 
-navBtn.addEventListener('click', () => {
-    if(navLinks.getAttribute('data-visible') === "false") {
+navBtn.addEventListener("click", () => {
+    if(navLinks.getAttribute("data-visible") === "false") {
         setVisible(true);
     } else {
         setVisible(false);
     }
 });
 
-navLinks.addEventListener('click', () => {
+navLinks.addEventListener("click", () => {
     setVisible(false);
 })
 
@@ -17,7 +18,11 @@ window.addEventListener("resize", () => {
     setVisible(false);
 });
 
+window.addEventListener("scroll", () => {
+    navBar.classList.toggle("fixed", window.scrollY > 0);
+})
+
 function setVisible(visible) {
-    navLinks.setAttribute('data-visible', visible);
-    navBtn.setAttribute('aria-expanded', visible);
+    navLinks.setAttribute("data-visible", visible);
+    navBtn.setAttribute("aria-expanded", visible);
 }
