b3b6ef990e38bed47cb37f413e455d3fb22440b9
JimiIT92 <jimiit92@gmail.com>
2022-04-17 22:52:37 +0200

    Set general layout

--- assets/js/home_typed.js
+++ assets/js/home_typed.js
@@ -1,8 +1,9 @@
-// new Typed('#typed', {
-//     stringsElement: '#typed-strings',
-//     typeSpeed: 50,
-//     backSpeed: 50,
-//     backDelay: 1500,
-//     shuffle: true,
-//     loop: true
-// });
+new Typed('#title', {
+    stringsElement: '#title-values',
+    typeSpeed: 50,
+    startDelay: 250,
+    backSpeed: 50,
+    backDelay: 1500,
+    shuffle: false,
+    loop: true
+});
